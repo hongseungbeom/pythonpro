b339c16687fdbe83e39afd6c8508b6f3b7ebaa35
hongseungbeom <65267629+hongseungbeom@users.noreply.github.com>
2023-12-13 10:26:16 +0900

    Update prob1.py

--- final/prob1.py
+++ final/prob1.py
@@ -1,1 +1,13 @@
+def frequency_analytic(s):
+  counts = {}
+  for alpb in s:
+    ltr = alpb
+    if ltr not in counts:
+      counts[ltr] = 0
+    counts[ltr] += 1 
+  for ltr in sorted(counts.keys()):
+    print(ltr, counts[ltr])
 
+if __name__ =="__main__":
+  msg = input('input your message :')
+  frequency_analytic(msg)
